0a0b54f265078b803b86cb74155a9de754ea369d
CST\YUY <yuy@canic.com>
2018-09-02 22:23:32 +0800

    1

---
+++ backend/all-in-one/src/applications/run.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+echo "...pull..."
+git pull
+
+echo "...stop container tourjpn..."
+docker stop tourjpn
+
+echo "...remove container tourjpn..."
+docker rm tourjpn
+
+echo "...remove image tourjprun..."
+docker rmi tourjprun
+
+echo "...set linux temp env..."
+CGO_ENABLED=0 GOOS=linux 
+
+echo "...building golang..."
+go build -a -installsuffix cgo  -tags netgo -o tourjprun
+
+echo "...building docker image..."
+docker build -t tourjprun:latest .
+
+echo "...start docker..."
+docker run -itd -p 805:805 -v /self/prod/images:/self/prod/aaa  --name tourjpn --network=yuyang_nw  tourjprun:latest
+
+echo "...done..."
+
